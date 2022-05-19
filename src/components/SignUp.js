3c0e51a8b4aae640a60cfd7de00a81e12b08796a
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-05-19 15:43:37 +1000

    Axios post request working

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,7 +2,7 @@ import React from 'react'
 import SignUpForm from './SignUpForm'
 function SignUp() {
   return (
-    <div>
+    <div className="container">
         <h1>VR GAME Signup </h1>
         <img src="http://via.placeholder.com/640x360" alt="" />
         <SignUpForm />
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,29 +1,58 @@
 import React, { useState } from 'react'
+import axios from 'axios';
+const SERVER_URL = "http://localhost:3000/users.json"
 
 function SignUpForm() {
 
 const [firstName, setFirstName] = useState('');
 const [lastName, setLastName] = useState('');
 const [email, setEmail] = useState('');
+const [response, setResponse] = useState('');
+
+const _handleFNChange = (e) => {
+    setFirstName(e.target.value);
+};
+
+const _handleLNChange = (e) => {
+    setLastName(e.target.value);
+};
+
+const _handleEMChange = (e) => {
+    setEmail(e.target.value);
+};
 
 const _handleSubmit = (event) => {
     event.preventDefault();
-    console.log('submit')
+    const user = {
+        firstname: firstName,
+        lastname: lastName,
+        email: email
+    }
+    axios.post(SERVER_URL, { user })
+    .then((response) => {
+        setResponse(response.data);
+     })
+     .then(console.log(response))
+    
+    // console.log({'user': {
+    //     firstName: firstName//     lastName: lastName,
+    //     email: email
+    // }})
 } 
 return (
     <div className="form">
         <form onSubmit={ _handleSubmit }>
             <label className="form-group">
                 Email:
-                <input className="form-control" type="email" required placeholder="Email" />
+                <input className="form-control" type="email" required placeholder="Email" onChange={ _handleEMChange }/>
             </label>
             <label className="form-group">
                 First Name:
-                <input className="form-control" type="text" required placeholder="First Name" />
+                <input className="form-control" type="text" required placeholder="First Name" onChange={ _handleFNChange }  />
             </label>
             <label className="form-group">
                 Last Name:
-                <input className="form-control" type="text" required placeholder="Last Name" />
+                <input className="form-control" type="text" required placeholder="Last Name" onChange={ _handleLNChange }/>
             </label>
             <button className="btn btn-block">Sign Up!</button>
         </form>
