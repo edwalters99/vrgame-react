be8454d120c81e7375bb1b35733f9cdb01f03d84
Daniel Martinez <danielmartinez@daniels-mbp.lan>
2022-05-19 17:42:22 +1000

    styling

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -40,23 +40,30 @@ const _handleSubmit = (event) => {
     // }})
 } 
 return (
-    <div className="form">
+    <div className="signupformContainer">
+    <h2 className="form-header">Join the guild!</h2>
+    <h3 className="form-subheader">Receive loot, the latest news & early-access
+    to the upcoming games.</h3>
+    <div className>
         <form onSubmit={ _handleSubmit }>
             <label className="form-group">
-                Email:
-                <input className="form-control" type="email" required placeholder="Email" onChange={ _handleEMChange }/>
+                First name*
+                <input className="form-control" type="text" required placeholder="So we can personalise your loot!" onChange={ _handleFNChange }  />
             </label>
             <label className="form-group">
-                First Name:
-                <input className="form-control" type="text" required placeholder="First Name" onChange={ _handleFNChange }  />
+                Last name*
+                <input className="form-control" type="text" required placeholder="Last name" onChange={ _handleLNChange }/>
             </label>
             <label className="form-group">
-                Last Name:
-                <input className="form-control" type="text" required placeholder="Last Name" onChange={ _handleLNChange }/>
+                Email address*
+                <input className="form-control" type="email" required placeholder="Don't worry we won't spam you" onChange={ _handleEMChange }/>
             </label>
-            <button className="btn btn-block">Sign Up!</button>
+            <button className="btn btn-block">Let's go!</button>
         </form>
+        <p className="errors">Oops, something doesn't look right!</p>
     </div>
+    
+</div>
   )
 }
 
