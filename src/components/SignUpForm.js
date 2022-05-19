e7d21482f213f8d4a5308fbb3558f9525934eb8a
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-05-19 23:42:50 +1000

    Validation functions added, conditional error messages on form submission

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import axios from 'axios';
-// const SERVER_URL = "http://localhost:3000/users.json"
-const SERVER_URL = "https://vrg-backend.herokuapp.com/users.json"
+const SERVER_URL = "http://localhost:3005/users.json"
+// const SERVER_URL = "https://vrg-backend.herokuapp.com/users.json"
+
 
 function SignUpForm() {
 
@@ -9,6 +10,11 @@ const [firstName, setFirstName] = useState('');
 const [lastName, setLastName] = useState('');
 const [email, setEmail] = useState('');
 const [response, setResponse] = useState('');
+const [serverError, setServerError] = useState(false);
+const [firstNameValid, setFirstNameValid] = useState(true);
+const [lastNameValid, setLastNameValid] = useState(true);
+const [emailValid, setEmailValid] = useState(true);
+
 
 const _handleFNChange = (e) => {
     setFirstName(e.target.value);
@@ -26,26 +32,67 @@ const clearForm = () => {
     setFirstName('');
     setLastName('');
     setEmail('');
+};
+
+const validateName = (name) => {
+    // A name cannot contain a number
+// A name cannot contain a special character
+// A name must be at least two letters
+    if (name.length < 2) {
+        return false;
+    }
+    return (/^[a-zA-Z]*$/).test(name);
+
+};
+
+
+const validateEmail = (email) => {
+//     An email address must contain exactly one @
+// An email address must contain at least one full stop (.)
+    const atSigns = email.split('').filter(char => char === '@').length;
+    const periods = email.split('').filter(char => char === '.').length;
+    return (atSigns === 1 && periods >= 1);
 }
+
 
 const _handleSubmit = (event) => {
     event.preventDefault();
+  
     const user = {
         firstname: firstName,
         lastname: lastName,
         email: email
     }
+    setFirstNameValid(validateName(user.firstname));
+    setLastNameValid(validateName(user.lastname));
+    setEmailValid(validateEmail(user.email));
+    
     axios.post(SERVER_URL, { user })
     .then((response) => {
         setResponse(response.data);
      })
-     .then(clearForm());
+     .then(clearForm())
+     .catch(function (error) {
+        if (error.response) {
+          // Request made and server responded
+          console.log(error.response.data);
+          setServerError(true);
+
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log(error.request);
+          setServerError(true);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+          setServerError(true);
+        }
     
-    // console.log({'user': {
-    //     firstName: firstName//     lastName: lastName,
-    //     email: email
-    // }})
-} 
+      });
+    } 
+
 return (
     <div className="signupformContainer">
         <h2 className="form-header">Join the guild!</h2>
@@ -67,7 +114,8 @@ return (
                 </label>
                 <button className="btn btn-block">Let's go!</button>
             </form>
-            <p className="errors">Oops, something doesn't look right!</p>
+            <p className="errors">{(!firstNameValid || !lastNameValid || !emailValid) && `Oops, something doesn't look right!`}</p>
+            <p className="errors">{serverError && `Sorry, we encountered a hiccup with our server!`}</p>
         </div>
     </div>
   )
